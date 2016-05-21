957a4c89ab04da2f3b19fe4a73afbbf44f72c151
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-05-21 19:28:16 +0200

    small update

--- HSP/ui.R
+++ HSP/ui.R
@@ -19,8 +19,8 @@ shinyUI(fluidPage(
          selectInput("hspgene", "HSP 40 / TP:",
                      c("MDM2", "TP53", "TP63", "TP73", "DNAJB1", "DNAJB2", "DNAJB4", "DNAJB5", "DNAJB6", "DNAJB9", "DNAJB11", "DNAJB12", "DNAJB13", "DNAJB14"),
                      "TP63"),
-         checkboxInput("median", "Median / 0 split", TRUE),
          checkboxInput("groups42", "4 groups / 2 groups", TRUE),
+         checkboxInput("median", "Median / 0 split", TRUE),
          plotOutput("distPlot", width = "100%", height = "350px")
     ),
     column(4,
