8ce5d75e69de842db7a046fe97ee696c2624a7df
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-05-21 16:19:12 +0200

    v2

--- HSP/server.R
+++ HSP/server.R
@@ -12,12 +12,20 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     cohort <- "TCGA Breast Cancer"
     hspgene = "DNAJB2"
-
-    df <- na.omit(clinical_expression_mut[clinical_expression_mut$X_cohort == input$cohort,])
-
     hspgene = input$hspgene
-    ggplot(df, aes_string(hspgene)) +
-      geom_histogram() + geom_vline(xintercept = 0, color="red", size=2)
+    
+    df <- na.omit(clinical_expression_mut[clinical_expression_mut$X_cohort == input$cohort,
+                                          c(hspgene), drop=FALSE])
+
+    pl <- ggplot(df, aes_string(hspgene)) +
+      geom_histogram()+
+      ggtitle(paste(hspgene, "in\n", cohort,"\n", nrow(df), "cases"))
+    if (input$median) {
+      pl <- pl + geom_vline(xintercept = 0, color="red", size=2) 
+    } else { 
+      pl <- pl + geom_vline(xintercept = median(df[,hspgene], na.rm = TRUE), color="red", size=2) 
+    }
+    pl
   })
   
   output$distPlot2 <- renderPlot({
@@ -25,47 +33,102 @@ shinyServer(function(input, output) {
     hspgene = "DNAJB2"
     
     df <- clinical_expression_mut[clinical_expression_mut$X_cohort == input$cohort,]
-    
     df <- df[df$X18475 == "1", ]
     
     hspgene = input$hspgene
-    df$bin <- cut(df[,hspgene], breaks = c(-100,0,100), labels = paste(hspgene,c("low", "high"), "mut53"))
+    if (input$median) {
+      df$bin <- cut(df[,hspgene], breaks = c(-100,0,100), labels = paste(hspgene,c("low", "high"), "/ mut53"))
+    } else { 
+      df$bin <- cut(df[,hspgene], breaks = c(-100,median(df[,hspgene], na.rm = TRUE),100), labels = paste(hspgene,c("low", "high"), "/ mut53"))
+    }
 
     df <- na.omit(df)
     model <- survfit(Surv(X_TIME_TO_EVENT,X_EVENT) ~ bin, data=df)
     pp <- pchisq(survdiff(Surv(X_TIME_TO_EVENT,X_EVENT) ~ bin, data=df)$chisq, 1, lower.tail = F)
 
-    ggsurvplot(model)$plot + ggtitle(paste0("p-value: ", signif(pp,2)))
-
+    ggsurvplot(model)$plot + 
+      ggtitle(paste0("High/Low ",hspgene, "\nOnly mut tp53\np-value: ", signif(pp,2))) +
+      theme(legend.position=c(0.2,0.1))
   })
   
+  # tylko HIGH
   output$distPlot3 <- renderPlot({
     df <- clinical_expression_mut[clinical_expression_mut$X_cohort == input$cohort,]
     
-    df$MDM2_TP53 = ifelse(df$X18475 == "1" & df$MDM2 > 0, 
-                          "MDM2_high_TP53_mut",
-                          "others")
-    
     hspgene = input$hspgene
-    df$bin <- cut(df[,hspgene], breaks = c(-100,0,100), labels = paste(hspgene,c("low", "high"), "mut53"))
-
-    df <- na.omit(df)
-    model <- survfit(Surv(X_TIME_TO_EVENT,X_EVENT) ~ MDM2_TP53 + bin, data=df)
-    pp <- pchisq(survdiff(Surv(X_TIME_TO_EVENT,X_EVENT) ~ MDM2_TP53 + bin, data=df)$chisq, 1, lower.tail = F)
+    if (input$median) {
+      df <- df[which(df[,hspgene] > 0),]
+      df$MDM2b <- cut(df[,"MDM2"], breaks = c(-100,0,100), labels = paste("MDM2",c("low", "high")))
+    } else { 
+      df <- df[which(df[,hspgene] > median(df[,hspgene], na.rm = TRUE)),]
+      df$MDM2b <- cut(df[,"MDM2"], breaks = c(-100,median(df[,"MDM2"], na.rm = TRUE),100), labels = paste("MDM2",c("low", "high")))
+    }
+    
+    df$TP53 = ifelse(df$X18475 == "1", "TP53 mut", "TP53 wild")
+    
+    df <- na.omit(df[,c("X_TIME_TO_EVENT", "X_EVENT", "TP53", "MDM2b")])
+    model <- survfit(Surv(X_TIME_TO_EVENT,X_EVENT) ~ TP53 + MDM2b, data=df)
+    pp <- pchisq(survdiff(Surv(X_TIME_TO_EVENT,X_EVENT) ~ TP53 + MDM2b, data=df)$chisq, 3, lower.tail = F)
     
     ggsurvplot(model)$plot + ggtitle(paste0("p-value: ", signif(pp,2))) +
-      theme(legend.position="right")
-    
+      ggtitle(paste0("Only HIGH ",hspgene, "\np-value: ", signif(pp,2))) +
+      theme(legend.position=c(0.3,0.15))
   })
   
   
+  # tylko LOW
+  output$distPlot4 <- renderPlot({
+    df <- clinical_expression_mut[clinical_expression_mut$X_cohort == input$cohort,]
+    
+    hspgene = input$hspgene
+    if (input$median) {
+      df <- df[which(df[,hspgene] <= 0),]
+      df$MDM2b <- cut(df[,"MDM2"], breaks = c(-100,0,100), labels = paste("MDM2",c("low", "high")))
+    } else { 
+      df <- df[which(df[,hspgene] <= median(df[,hspgene], na.rm = TRUE)),]
+      df$MDM2b <- cut(df[,"MDM2"], breaks = c(-100,median(df[,"MDM2"], na.rm = TRUE),100), labels = paste("MDM2",c("low", "high")))
+    }
+    
+    df$TP53 = ifelse(df$X18475 == "1", "TP53 mut", "TP53 wild")
+    
+    df <- na.omit(df[,c("X_TIME_TO_EVENT", "X_EVENT", "TP53", "MDM2b")])
+    model <- survfit(Surv(X_TIME_TO_EVENT,X_EVENT) ~ TP53 + MDM2b, data=df)
+    pp <- pchisq(survdiff(Surv(X_TIME_TO_EVENT,X_EVENT) ~ TP53 + MDM2b, data=df)$chisq, 3, lower.tail = F)
+    
+    ggsurvplot(model)$plot + ggtitle(paste0("p-value: ", signif(pp,2))) +
+      ggtitle(paste0("Only LOW ",hspgene, "\np-value: ", signif(pp,2))) +
+      theme(legend.position=c(0.3,0.15))
+  })
+  
+  # tylko 
+  output$distPlot5 <- renderPlot({
+    df <- clinical_expression_mut[clinical_expression_mut$X_cohort == input$cohort,]
+    
+    hspgene = input$hspgene
+    if (input$median) {
+      df$gene <- cut(df[,hspgene], breaks = c(-100,0,100), labels = paste(hspgene,c("low", "high")))
+      df$MDM2b <- cut(df[,"MDM2"], breaks = c(-100,0,100), labels = paste("MDM2",c("low", "high")))
+    } else { 
+      df$gene <- cut(df[,hspgene], breaks = c(-100,median(df[,hspgene], na.rm = TRUE),100), labels = paste(hspgene,c("low", "high")))
+      df$MDM2b <- cut(df[,"MDM2"], breaks = c(-100,median(df[,"MDM2"], na.rm = TRUE),100), labels = paste("MDM2",c("low", "high")))
+    }
+    
+    df$TP53 = ifelse(df$X18475 == "1", "TP53 mut", "TP53 wild")
+    
+    df <- na.omit(df[,c("X_TIME_TO_EVENT", "X_EVENT", "TP53", "MDM2b", "gene")])
+    df$MDM2_TP53 <- factor(paste(df$MDM2b, df$TP53))
+    tmp <- data.frame(table(df$MDM2_TP53, df$gene))
+    
+    ggplot(tmp, aes(Var1, y=Freq, fill=Var2)) + geom_bar(stat="identity") +
+      coord_flip() + theme(legend.position="bottom") + xlab("") + ylab("")
+  })
   
 })
 
 
 
 
-# 
+
 # clinical <- clinical.cb[,c("X_PATIENT", "X_TIME_TO_EVENT", "X_EVENT", "X_cohort")]
 # clinical$X_PATIENT <- substr((clinical$X_PATIENT), 1, 12)
 # clinical$X_PATIENT <- gsub(clinical$X_PATIENT, pattern="-", replacement=".")
@@ -80,10 +143,11 @@ shinyServer(function(input, output) {
 # expression$X_PATIENT <- gsub(expression$X_PATIENT, pattern="-", replacement=".")
 # 
 # # mutation
-# clinical_expression <- merge(clinical, expression[,c("X_PATIENT", "DNAJB1", "DNAJB2", "DNAJB4", "DNAJB5", "DNAJB6", "DNAJB9", "DNAJB11", "DNAJB12", "DNAJB13", "DNAJB14", "MDM2", "TP73", "TP63")],  by="X_PATIENT", all.x=TRUE)
+# clinical_expression <- merge(clinical, expression[,c("X_PATIENT", "TP63", "TP73", "DNAJB1", "DNAJB2", "DNAJB4", "DNAJB5", "DNAJB6", "DNAJB9", "DNAJB11", "DNAJB12", "DNAJB13", "DNAJB14", "MDM2", "TP73", "TP63")],  by="X_PATIENT", all.x=TRUE)
 # 
 # TP53 <- mutation.cb[grep(mutation.cb[,1], pattern="TP53$", value = FALSE),-1]
 # TP53v <- data.frame(X_PATIENT = substr(names(TP53), 1, 12),TP53=t(TP53))
 # 
 # clinical_expression_mut <- merge(clinical_expression, TP53v, by="X_PATIENT", all.x=TRUE)
 # 
+# save(clinical_expression_mut, file="clinical_expression_mut.rda")
--- HSP/ui.R
+++ HSP/ui.R
@@ -7,28 +7,28 @@ shinyUI(fluidPage(
   titlePanel("HSP + MDM2 + tp53"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       selectInput("cohort", "Cancer:",
-                   c("TCGA Acute Myeloid Leukemia", "TCGA Bladder Cancer", "TCGA Breast Cancer", 
-                     "TCGA Colon Cancer", "TCGA Endometrioid Cancer", "TCGA Formalin Fixed Paraffin-Embedded Pilot Phase II", 
-                     "TCGA Glioblastoma", "TCGA Head and Neck Cancer", "TCGA Kidney Clear Cell Carcinoma", 
-                     "TCGA Lung Adenocarcinoma", "TCGA Lung Squamous Cell Carcinoma", 
-                     "TCGA Ovarian Cancer", "TCGA Rectal Cancer"),
-                   "TCGA Breast Cancer"),
-       selectInput("hspgene", "HSP 40:",
-                   c("DNAJB1", "DNAJB2", "DNAJB4", "DNAJB5", "DNAJB6", "DNAJB9", "DNAJB11", "DNAJB12", "DNAJB13", "DNAJB14"),
-                   "DNAJB1"),
-       selectInput("TP", "TP:",
-                   c("TP63", "TP73"),
-                   "TP63")
+  fluidRow(
+    column(3,
+         selectInput("cohort", "Cancer:",
+                     c("TCGA Acute Myeloid Leukemia", "TCGA Bladder Cancer", "TCGA Breast Cancer", 
+                       "TCGA Colon Cancer", "TCGA Endometrioid Cancer", "TCGA Formalin Fixed Paraffin-Embedded Pilot Phase II", 
+                       "TCGA Glioblastoma", "TCGA Head and Neck Cancer", "TCGA Kidney Clear Cell Carcinoma", 
+                       "TCGA Lung Adenocarcinoma", "TCGA Lung Squamous Cell Carcinoma", 
+                       "TCGA Ovarian Cancer", "TCGA Rectal Cancer"),
+                     "TCGA Breast Cancer"),
+         selectInput("hspgene", "HSP 40 / TP:",
+                     c("TP63", "TP73", "DNAJB1", "DNAJB2", "DNAJB4", "DNAJB5", "DNAJB6", "DNAJB9", "DNAJB11", "DNAJB12", "DNAJB13", "DNAJB14"),
+                     "TP63"),
+         checkboxInput("median", "Median / 0 split", TRUE),
+         plotOutput("distPlot", width = "100%", height = "350px")
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot3"),
-      plotOutput("distPlot2"),
-      plotOutput("distPlot")
+    column(4,
+           plotOutput("distPlot2", width = "100%", height = "400px"),
+           plotOutput("distPlot5", width = "100%", height = "400px")
+    ),
+    column(4,
+           plotOutput("distPlot3", width = "100%", height = "400px"),
+           plotOutput("distPlot4", width = "100%", height = "400px")
     )
   )
 ))
